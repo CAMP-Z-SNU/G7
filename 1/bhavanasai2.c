55573eccf19c33e67cfa66f7c4986970490e9ce8
Bhavanasai2006 <bhavanasaisattenapalli@gmail.com>
2024-09-04 21:52:12 +0530

    Add files via upload

---
+++ 1/bhavanasai2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num1;
+    int num2;
+    printf("enter one number:");
+    scanf("%d",&num1);
+    printf("enter second number:");
+    scanf("%d",&num2);
+    printf("sum of the numbers is:%d", num1+num2);
+    return 0;
+}
---
+++ 1/subtraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num1;
+    int num2;
+    printf("enter one number:");
+    scanf("%d",&num1);
+    printf("enter second number:");
+    scanf("%d",&num2);
+    printf("by subtracting second no. from first we get:%d", num1-num2);
+    return 0;
+}
