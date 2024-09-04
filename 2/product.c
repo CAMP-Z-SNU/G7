64b41911e04aed26b9709ccc439dfdc280e89881
Akshaya0603 <akshayapokuri1212@gmail.com>
2024-09-04 21:51:31 +0530

    Add files via upload

---
+++ 2/product.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("the first number");
+    scanf("%d",&a);
+    printf("the second number");
+    scanf("%d",&b);
+    printf("the difference of two number %d",a*b);
+    return 0;
+}
