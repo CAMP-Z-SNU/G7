5a14c7f9d0abf348436e3256bbf74c177d74ccbd
nivriti25 <nivriti.varada@gmail.com>
2024-09-09 21:48:04 +0530

    Add files via upload

---
+++ 3/factorial.c
@@ -0,0 +1,27 @@
+//program to find factorial of number using recursion 
+#include <stdio.h>
+
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1); 
+    }
+}
+
+int main() {
+    int number;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+    
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+    
+        printf("Factorial of %d is %d\n", number, factorial(number));
+    }
+
+    return 0;
+}
+// Difficulty level: 45
