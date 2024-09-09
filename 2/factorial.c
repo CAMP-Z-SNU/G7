6fb6526512713c3fd6598b9a395a3b2a19b453e8
Akshaya0603 <akshayapokuri1212@gmail.com>
2024-09-09 21:50:00 +0530

    Add files via upload

---
+++ 2/factorial.c
@@ -0,0 +1,31 @@
+/*
+The question is write a program to check the factorial
+*/
+#include <stdio.h>
+
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1); 
+    }
+}
+
+int main() {
+    int number;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+    
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+    
+        printf("Factorial of %d is %llu\n", number, factorial(number));
+    }
+
+    return 0;
+}
+/*
+difficulty level is 45
+*/
