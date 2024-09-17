8506222539adc5513aeef99e6b9440a9c843b39c
nivriti25 <nivriti.varada@gmail.com>
2024-09-17 20:54:35 +0530

    Add files via upload

--- 3/hello.c
+++ 3/hello.c
@@ -1,11 +1,26 @@
-#include<stdio.h>
-int main()
- {
+#include <stdio.h>
 
-    int A, B, diff;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &A, &B);
-    diff = A - B;
-    printf("diff: %d", diff);
+int fibonacci(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    
+    int prev1 = 0, prev2 = 1, current;
+    
+    for (int i = 2; i <= n; i++) {
+        current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+    
+    return prev2;
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    
+    printf("The %dth Fibonacci number is: %d\n", n, fibonacci(n));
+    
     return 0;
 }
